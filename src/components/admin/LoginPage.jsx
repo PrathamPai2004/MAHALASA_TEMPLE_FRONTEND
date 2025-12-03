d98eb4751e0d2304f36db6198cdc13d019dee6ed
Pratham_Pai <122979370+PrathamPai2004@users.noreply.github.com>
2025-12-04 00:24:41 +0530

    Refractored Login page after connecting to Database and handling secure navigation in the login page

--- src/components/admin/LoginPage.jsx
+++ src/components/admin/LoginPage.jsx
@@ -1,42 +1,69 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Dashboard from "./Dashboard.jsx";
 
 
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  
+  const [dashboard,setDashboard]=useState(false);
+  const [message,setMessage] = useState("");
   
  const navigate = useNavigate();
-  const handleSubmit = async(e) => {
+ const handleSubmit = async (e)=>{
     e.preventDefault();
     console.log("Username:", username);
     console.log("Password:", password);
-
-
     try{
-      const response = await axios.post('https://mahalasa-temple-backend.onrender.com/admin-login',{username,password});
-        if (response.data.message === "success") {
+      const res = await axios.post('https://mahalasa-temple-backend.onrender.com/admin-login',{username,password});
+      console.log(res.data.message)
+      if(res.data.message === "success"){``
         localStorage.setItem("isLoggedIn", "true");
-        navigate(`dashboard/${response.data.username}`);
-  }
-
-      // navigate(`dashboard/${response.data.username}`);
+        setDashboard(true);
+        // navigate(`/admin/dashboard/${res.data.username}`);
+      }
+      else{
+        setMessage("Invalid Credentials");
+      }
     }
-
     catch(error){
       console.log("Admin login error : "+error.message)
+      setMessage("An error occurred. Please try again.");
     }
-    // Add your login logic here
-  };
+ }
+  // const handleSubmit = async(e) => {
+  //   e.preventDefault();
+  //   console.log("Username:", username);
+  //   console.log("Password:", password);
+    
 
+  //   try{
+  //     const response = await axios.post('https://mahalasa-temple-backend.onrender.com/admin-login',{username,password});
+  //       if (response.data.message === "success") {
+  //       localStorage.setItem("isLoggedIn", "true");
+  //       navigate(`dashboard/${response.data.username}`);
+  // }
+
+  //     // navigate(`dashboard/${response.data.username}`);
+  //   }
+
+  //   catch(error){
+  //     console.log("Admin login error : "+error.message)
+  //   }
+  //   // Add your login logic here
+  // };
+
+  if(dashboard){
+    return <Dashboard/>
+  }
   return (
     <>
       <h2 style={{textAlign:"center"}}>Admin  Page of Mahalasa</h2>
       <p>Please login your username as "abc" and Passsword "abc" as the site is under Demo</p>
     <div style={{ maxWidth: "1000px",width:"40vw", margin: "50px auto", padding: "20px", border: "1px solid #ccc", borderRadius: "8px" }}>
-
+      
+      {message && <p style={{ color: "red", textAlign: "center" }}>{message}</p>}
       <form onSubmit={handleSubmit}>
         <div style={{ marginBottom: "15px" }}>
           <label htmlFor="username">Username</label><br />
